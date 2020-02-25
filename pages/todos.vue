23114ba00b4eb6954ae06ca8d578d55aac0c2267
ymdma <m.yamada1985@gmail.com>
2020-02-25 21:53:05 +0900

    first_commit

---
+++ pages/todos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    created: function() {
+      this.$store.dispatch('todos/init')
+    }
+  }
+</script>
+
+
+<style>
+
+</style>
