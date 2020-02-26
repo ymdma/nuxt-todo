bdcd632745ce1212199105a7fc37b186fc5d2362
ymdma <m.yamada1985@gmail.com>
2020-02-26 22:41:22 +0900

    app-basic

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase'
-
+// firebaseの初期化に必要なパラメータをオブジェクト形式で用意
 const config = {
   // databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID
+  // 適時 APIキーなどを追記
 }
-
+// 二重の初期化を防ぐ条件分岐を挟む
+// initializeAppメソッドを呼び出して初期化
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
 }
--- store/todos.js
+++ store/todos.js
@@ -1,38 +1,44 @@
-import firebase from '~/plugins/firebase'
-import { firestoreAction } from 'vuexfire'
+import firebase from '~/plugins/firebase' // firebaseの初期化設定ファイル
+import { firestoreAction } from 'vuexfire' // vuecfireが用意しているfirestoreActionを呼び出す
 
+// データベースの設定
 const db = firebase.firestore()
-const todosRef = db.collection('todos')
-
+const todosRef = db.collection('todos') // "collectionはtodosを使う" という設定
+// ステートの定義
+// todo一覧を管理するtodosを配列で定義しておく
 export const state = () => ({
   todos: []
 })
 
 export const actions = {
-  init: firestoreAction(({ bindFirestoreRef }) => {
-    bindFirestoreRef('todos', todosRef)
+
+  init: firestoreAction(({ bindFirestoreRef }) => { // Action呼出 引数は、本来なら第一引数にはcontextが入るが、こう書くとこれのみ受け取れる
+    bindFirestoreRef('todos', todosRef) //bind(関連付け)したいデータの名前'todos'とコレクションへの参照(2)を渡すとstate.todos(上のステート)にcloudfirestoreのデータが関連付けされる
   }),
-  add: firestoreAction((context, name) => {
-    if(name.trim()) {
-      todosRef.add({
+  // addは、todoの追加をする時に呼ばれるアクション
+  add: firestoreAction((context, name) => { // 第一引数にcontext(不使用)、第二引数にtodoの名前を受け取る
+    if(name.trim()) { // 入力が空白でないか確認。空白の場合無効に。
+      todosRef.add({ // todosRefのaddメソッドを使ってデータをfirestoreに登録。
         name: name,
-        done: false,
-        created: firebase.firestore.FieldValue.serverTimestamp()
+        done: false, // タスクの完了・未完了
+        created: firebase.firestore.FieldValue.serverTimestamp() // 作成時間。正規化はコンポーネント側で
       })
     }
   }),
-  remove: firestoreAction((context, id) => {
-    todosRef.doc(id).delete()
+  // todoを削除するアクション
+  remove: firestoreAction((context, id) => { // idはremoveを呼ぶ時に引数として使用
+    todosRef.doc(id).delete() // idが一致するデータを削除
   }),
+  // チェックボックス操作時のアクション todoの完了・未完了を管理するための機能
   toggle: firestoreAction((context, todo) => {
     todosRef.doc(todo.id).update({
-      done: !todo.done
+      done: !todo.done // 現在の値を反転して使用
     })
   })
 }
 
 
-
+// ゲッター Firebaseからデータを取ってくるとデフォでID順になるので、createdを基準にしてソート
 export const getters = {
   orderdTodos: state => {
     return _.sortBy(state.todos, 'created')
